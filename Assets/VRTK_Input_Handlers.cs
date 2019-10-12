d86ca6deef95d5eed688d2d6c03dba895f3c421f
jtrinh3 <j.trinh227@gmail.com>
2019-10-12 06:03:50 -0500

    made my stuff interactable, now trying to get the button on boombox working

---
+++ Assets/VRTK_Input_Handlers.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class VRTK_Input_Handlers : MonoBehaviour
+{
+    public bool boombox_status = false;
+    AudioSource audioData;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void toggle_boombox()
+    {
+        audioData = GameObject.Find("Boombox Button").GetComponent<AudioSource>();
+        if(boombox_status == false) //music originally off
+        {
+            audioData.Play(); //turnt
+            boombox_status = true;
+        }
+        else //music was on
+        {
+            audioData.Pause(); //turnt down (for what!?)
+            boombox_status = false;
+        }
+    }
+
+}
